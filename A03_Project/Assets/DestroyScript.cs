9f3afd330005880f4c1ea6ed93701e072d0ea512
ldcduc <ldcduc@apcs.vn>
2019-06-12 21:34:01 +0700

    Submit A03

---
+++ A03_Project/Assets/DestroyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject characterObject;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject == characterObject) {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
---
+++ A03_Project/Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator; 
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    public bool isGrounded = false;
+
+    //bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        /*if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }*/
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 20f), ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        //jump = false;
+    }
+}
